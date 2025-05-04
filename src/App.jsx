b327fccbd9066f39b031c2e1bf6b6b3d300b18d6
murtaza192168 <murtazacloudwork1252@gmail.com>
2025-05-04 18:56:43 +0400

    Featured Inventory table on Front end

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import { useState } from 'react'
-
+import InventoryTable from "./components/InventoryTable";
+import { Container } from "@mui/material";
 
 
 function App() {
   return(
-    <></>
+    <Container>
+      <h1>Inventory Management</h1>
+      <InventoryTable />
+    </Container>
   )
 }
 
